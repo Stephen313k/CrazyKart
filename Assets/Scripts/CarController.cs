11589b6cff7dba0aa7bee23d322804b56a754979
skli420 <iawesomely@gmail.com>
2022-05-04 20:01:44 +0100

    myo-armband-movement-for-car

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -2,30 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarController : MonoBehaviour {
+using LockingPolicy = Thalmic.Myo.LockingPolicy;
+using Pose = Thalmic.Myo.Pose;
+using UnlockType = Thalmic.Myo.UnlockType;
+using VibrationType = Thalmic.Myo.VibrationType;
 
+
+public class CarController : MonoBehaviour {
+    public static float speed = 3;
 
     public float carSpeed;
     public float maxPos = 2.5f;
 
     Vector3 position;
 
-	// Use this for initialization
-	void Start () {
+    public GameObject myo = null;
+
+    void Move()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(moveHorizontal, 0) * speed;
+        ThalmicMyo thalmicMyo = myo.GetComponent<ThalmicMyo>();
+       
+            if (thalmicMyo.pose == Pose.WaveOut)
+            {
+                transform.position += Vector3.right * speed * Time.deltaTime;
+
+            }
+
+            else if (thalmicMyo.pose == Pose.WaveIn)
+            {
+               //position.x += Input.GetAxis("Horizontal") * carSpeed * Time.deltaTime;
+
+                transform.position += Vector3.left * speed * Time.deltaTime;
+
+             //   ExtendUnlockAndNotifyUserAction(thalmicMyo);
+            }
+        
+    }
+
+    // Use this for initialization
+    void Start () {
         position = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        //key press
-        position.x += Input.GetAxis("Horizontal") * carSpeed * Time.deltaTime;
-
+        Move();
+   
         //limit car from going off road
-        position.x = Mathf.Clamp(position.x, -maxPos, maxPos);
+         position.x = Mathf.Clamp(position.x, -maxPos, maxPos);
 
         //move
-        transform.position = position;
-	}
+       //  transform.position = position;
+    }
 
     //collisions hitting enemy car
     void OnCollisionEnter2D(Collision2D col)
