c6b69f4dc6a8fae2cd5d1be5ed41f4dc38635249
skli420 <iawesomely@gmail.com>
2022-05-05 02:29:40 +0100

    myo-armband-movement-for-car

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -21,6 +21,11 @@ public class CarController : MonoBehaviour {
     //for myo armband
     public GameObject myo = null;
 
+    //bool for android platform
+    bool currentPlatformAndroid = false;
+
+    Rigidbody2D rb;
+
     //myo armband
     void Move()
     {
@@ -45,21 +50,48 @@ public class CarController : MonoBehaviour {
 
     private void Awake()
     {
+        rb = GetComponent<Rigidbody2D>();
+
+        //if platform is android set to true
+        #if UNITY_ANDROID
+                currentPlatformAndroid = true;
+        #else
+                currentPlatformAndroid = false;
+        #endif
+
         //play car sound
         am.carSound.Play();
 
     }
-    // Use this for initialization
     void Start () {
+        // initialization
         position = transform.position;
+
+        if (currentPlatformAndroid == true)
+        {
+            Debug.Log("Android");
+        }
+        else
+        {
+            Debug.Log("Windows");
+        }
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Move();
-   
-        //limit car from going off road
-         position.x = Mathf.Clamp(position.x, -maxPos, maxPos);
+        if(currentPlatformAndroid == true) //android
+        {
+            //android movement
+            //  TouchMove();
+            AccelerometerMove();
+        }
+        else //windows
+        {   //myo-armband movement
+            Move();
+        }
+         
 
     }
 
@@ -69,9 +101,74 @@ public class CarController : MonoBehaviour {
         if(col.gameObject.tag == "Enemy")
         {
             //delete the car, display menu and stop car sound
-            Destroy(gameObject);
+            gameObject.SetActive(false);
             ui.GameOver();
             am.carSound.Stop();
+        }
+    }
+
+    //ANDROID ONLY BELOW
+
+
+    public void TouchMove()
+    {
+        if(Input.touchCount > 0)
+        {
+            //first touch is stored
+            Touch touch = Input.GetTouch(0);
+            //touch position left, car goes left. determine left or right on screen with greater or less than middlepoint
+            float middle = Screen.width / 2;
+
+            if(touch.position.x < middle && touch.phase == TouchPhase.Began)
+            {
+                MoveLeft();
+            }
+
+            else if (touch.position.x > middle && touch.phase == TouchPhase.Began)
+            {
+                MoveRight();
+            }          
+        }
+        else
+            {
+                SetVelocityZero();
+            }
+    }
+
+    //android move left button
+    public void MoveLeft()
+    {
+        //carspeed is negatative for left
+        rb.velocity = new Vector2(-carSpeed, 0);
+    }
+    //android move right button
+    public void MoveRight()
+    {
+        rb.velocity = new Vector2(carSpeed, 0);
+
+    }
+    //android stops moving
+    public void SetVelocityZero()
+    {
+        rb.velocity = Vector2.zero;
+    }
+
+    public void AccelerometerMove()
+    {
+        //getting x axis
+        float x = Input.acceleration.x;
+        Debug.Log("x = " + x);
+        //check value of x, negative left. positive right
+        if(x < -0.1f)
+        {
+            MoveLeft();
+        }else if(x > 0.1f)
+        {
+            MoveRight();
+        }
+        else //if phone is not tilted 
+        {
+            SetVelocityZero(); //dont move car
         }
     }
 }
